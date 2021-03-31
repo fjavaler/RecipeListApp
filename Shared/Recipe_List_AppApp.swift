a2a4186c2401f03e0428cc6ac623bbba1e4cf788
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-31 10:48:38 -0600

    Initial Commit

---
+++ Shared/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Shared
+//
+//  Created by Fred Javalera on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
