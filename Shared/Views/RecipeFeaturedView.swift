10cf89fab0e7f4b2cdd8aa7fbe05c6546a2af615
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-08 11:43:04 -0600

    Fixed issue with text being cut off of dish name on RecipeFeaturedView.swift.

--- Shared/Views/RecipeFeaturedView.swift
+++ Shared/Views/RecipeFeaturedView.swift
@@ -9,67 +9,71 @@
 import SwiftUI
 
 struct RecipeFeaturedView: View {
-  
-  @EnvironmentObject var viewModel: RecipeViewModel
-  
-  var body: some View {
     
-    VStack(alignment: .leading, spacing: 0) {
-      
-      Text("Featured Recipes")
-        .bold()
-        .padding(.leading)
-        .padding(.top, 40)
-        .font(.largeTitle)
-      
-      GeometryReader(content: { geometry in
-        TabView {
-          
-          // Loop through each recipe.
-          ForEach (0..<viewModel.recipes.count) {index in
+    @EnvironmentObject var viewModel: RecipeViewModel
+    
+    var body: some View {
+        
+        VStack(alignment: .leading, spacing: 0) {
             
-            // Only show those that should be featured.
-            if viewModel.recipes[index].featured == true {
-              
-              // Recipe card.
-              ZStack {
-                Rectangle()
-                  .foregroundColor(.white)
-                VStack (spacing:0) {
-                  Image(viewModel.recipes[index].image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .clipped()
-                  Text(viewModel.recipes[index].name)
-                    .padding(5)
+            Text("Featured Recipes")
+                .bold()
+                .padding(.leading)
+                .padding(.top, 40)
+                .font(.largeTitle)
+            
+            GeometryReader(content: { geometry in
+                TabView {
+                    
+                    // Loop through each recipe.
+                    ForEach (0..<viewModel.recipes.count) {index in
+                        
+                        // Only show those that should be featured.
+                        if viewModel.recipes[index].featured == true {
+                            
+                            // Recipe card.
+                            GeometryReader(content: { geometry in
+                                ZStack {
+                                    Rectangle()
+                                        .foregroundColor(.white)
+                                    VStack (spacing:0) {
+                                        Image(viewModel.recipes[index].image)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: geometry.size.width, height: geometry.size.height/1.15)
+                                            .clipped()
+                                        Text(viewModel.recipes[index].name)
+                                            .padding(5)
+                                    }
+                                }
+                                .frame(width: geometry.size.width - 40, height: geometry.size.height - 50, alignment: .center)
+                                .cornerRadius(15)
+                                .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
+                                .padding(.leading, 20)
+                            })
+                        }
+                    }
                 }
-              }
-              .frame(width: geometry.size.width - 40, height: geometry.size.height - 100, alignment: .center)
-              .cornerRadius(15)
-              .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            })
+            
+            VStack (alignment: .leading, spacing: 10){
+                Text("Preparation Time:")
+                    .font(.headline)
+                Text("1 hour")
+                Text("Highlights")
+                    .font(.headline)
+                Text("Healthy, Hearty")
             }
-          }
+            .padding([.leading, .bottom])
         }
-        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
-      })
-      
-      VStack (alignment: .leading, spacing: 10){
-        Text("Preparation Time:")
-          .font(.headline)
-        Text("1 hour")
-        Text("Highlights")
-          .font(.headline)
-        Text("Healthy, Hearty")
-      }
-      .padding([.leading, .bottom])
     }
-  }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
-  static var previews: some View {
-    RecipeFeaturedView()
-      .environmentObject(RecipeViewModel())
-  }
+    static var previews: some View {
+        RecipeFeaturedView()
+            .environmentObject(RecipeViewModel())
+    }
 }
